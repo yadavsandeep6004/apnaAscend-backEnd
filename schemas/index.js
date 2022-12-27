c5536d234f1032229a5fec13721b84b3fc45b308
Sarveshwar Bansal <paotabansal@gmail.com>
2022-12-27 11:04:40 +0530

    keyOffering Schema

--- schemas/index.js
+++ schemas/index.js
@@ -3,6 +3,6 @@ import upcomingCourses from "./upcomingCourses";
 import FrequentlyAskedQuestions from "./FrequentlyAskedQuestions";
 import SuccessStory from "./SuccessStory";
 import CoursesHeroSection from "./CoursesHeroSection";
+import KeyOffering from "./KeyOffering";
 
-
-export const schemaTypes = [activeCourses, upcomingCourses, FrequentlyAskedQuestions, SuccessStory, CoursesHeroSection]
+export const schemaTypes = [activeCourses, upcomingCourses, FrequentlyAskedQuestions, SuccessStory, CoursesHeroSection,KeyOffering]
